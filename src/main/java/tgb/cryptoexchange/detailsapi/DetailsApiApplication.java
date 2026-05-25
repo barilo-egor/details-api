7e28eb14ec76b4bf511ccffa877d0564927125e9
vdovin <V.Vdovin@isida.by>
2026-05-25 10:32:16 +0300

    init

---
+++ src/main/java/tgb/cryptoexchange/detailsapi/DetailsApiApplication.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.detailsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetailsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tgb/cryptoexchange/detailsapi/DetailsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.detailsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
